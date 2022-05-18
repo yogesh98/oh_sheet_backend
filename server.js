3ba2a5c18c9eba29e33a0a632b956485ff443403
yogesh98 <yogesh.patel0221@gmail.com>
2022-05-17 23:07:34 -0400

    (update) socket emission update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
+	socket.emit("id", socket.id);
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
